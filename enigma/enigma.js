b3d4d8fd01f83bf2438c0dd3deda9750da3e54cc
TrapinchO <67415128+TrapinchO@users.noreply.github.com>
2020-11-01 15:12:12 +0100

    patch 1.1.0
    added working plugboard

--- enigma/enigma.js
+++ enigma/enigma.js
@@ -1,5 +1,3 @@
-//alert("WARNING: This page is not finished, however it works.")
-
 var abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var rotors = {
@@ -40,47 +38,81 @@ function enigma(character) {
 
     // get the variables
     // check if they are valid number
-    // check if they are within 1-6
+    // check if they are within range 1-6
     // ---------- rotor selection ----------
     rotor_sel[0] = parseInt(document.getElementById('rotor1').value);
-    if (rotor_sel[0] != 0 && !rotor_sel[0]) { out = out.concat("* Rotor 1: Please enter a valid number<br>"); }
-    else if (rotor_sel[0] < 1 || rotor_sel[0] > 6) { out = out.concat("* Rotor 1: Please enter valid rotor<br>")}
+    if (rotor_sel[0] != 0 && !rotor_sel[0]) { out = out.concat("Rotor 1: Please enter a valid number<br>"); }
+    else if (rotor_sel[0] < 1 || rotor_sel[0] > 6) { out = out.concat("Rotor 1: Please enter valid rotor<br>")}
 
     rotor_sel[1] = parseInt(document.getElementById('rotor2').value);
-    if (rotor_sel[1] != 0 && !rotor_sel[1]) { out = out.concat("* Rotor 2: Please enter a valid number<br>"); }
-    else if (rotor_sel[1] < 1 || rotor_sel[1] > 6) { out = out.concat("* Rotor 2: Please enter valid rotor<br>")}
+    if (rotor_sel[1] != 0 && !rotor_sel[1]) { out = out.concat("Rotor 2: Please enter a valid number<br>"); }
+    else if (rotor_sel[1] < 1 || rotor_sel[1] > 6) { out = out.concat("Rotor 2: Please enter valid rotor<br>")}
 
     rotor_sel[2] = parseInt(document.getElementById('rotor3').value);
-    if (rotor_sel[2] != 0 && !rotor_sel[2]) { out = out.concat("* Rotor 3: Please enter a valid number<br>"); }
-    else if (rotor_sel[2] < 1 || rotor_sel[2] > 6) { out = out.concat("* Rotor 3: Please enter valid rotor<br>")}
+    if (rotor_sel[2] != 0 && !rotor_sel[2]) { out = out.concat("Rotor 3: Please enter a valid number<br>"); }
+    else if (rotor_sel[2] < 1 || rotor_sel[2] > 6) { out = out.concat("Rotor 3: Please enter valid rotor<br>")}
 
-    if (rotor_sel[0] == rotor_sel[1] == rotor_sel[2]) { out = out.concat("* All rotors are the same<br>"); }
-    else if (rotor_sel[0] == rotor_sel[1]) { out = out.concat("* 1st and 2nd rotors are the same<br>"); }
-    else if (rotor_sel[0] == rotor_sel[2]) { out = out.concat("* 1st and 3rd rotors are the same<br>"); }
-    else if (rotor_sel[1] == rotor_sel[2]) { out = out.concat("* 2nd and 3rd rotors are the same<br>"); }
+    if (rotor_sel[0] == rotor_sel[1] == rotor_sel[2]) { out = out.concat("All rotors are the same<br>"); }
+    else if (rotor_sel[0] == rotor_sel[1]) { out = out.concat("1st and 2nd rotors are the same<br>"); }
+    else if (rotor_sel[0] == rotor_sel[2]) { out = out.concat("1st and 3rd rotors are the same<br>"); }
+    else if (rotor_sel[1] == rotor_sel[2]) { out = out.concat("2nd and 3rd rotors are the same<br>"); }
 
 
     // ---------- rotor position ----------
+    // get the value
+    // test if number
+    // test if within range 1-26
     rotor_pos[0] = parseInt(document.getElementById('rotor_pos1').value);
-    if (!rotor_pos[0]) { out = out.concat("* Rotor position 1: Please enter a valid number<br>"); }
-    else if (rotor_pos[0] < 1 || rotor_pos[0] > 26) { out = out.concat("* Rotor position 1: Please enter valid position<br>"); }
+    if (!rotor_pos[0]) { out = out.concat("Rotor position 1: Please enter a valid number<br>"); }
+    else if (rotor_pos[0] < 1 || rotor_pos[0] > 26) {
+        out = out.concat("Rotor position 1: Please enter valid position<br>"); }
 
     rotor_pos[1] = parseInt(document.getElementById('rotor_pos2').value);
-    if (!rotor_pos[1]) { out = out.concat("* Rotor position 2: Please enter a valid number<br>"); }
-    else if (rotor_pos[1] < 1 || rotor_pos[1] > 26) { out = out.concat("* Rotor position 2: Please enter valid position<br>"); }
+    if (!rotor_pos[1]) { out = out.concat("Rotor position 2: Please enter a valid number<br>"); }
+    else if (rotor_pos[1] < 1 || rotor_pos[1] > 26) {
+        out = out.concat("Rotor position 2: Please enter valid position<br>"); }
 
     rotor_pos[2] = parseInt(document.getElementById('rotor_pos3').value);
-    if (!rotor_pos[2]) { out = out.concat("* Rotor position 3: Please enter a valid number<br>"); }
-    else if (rotor_pos[2] < 1 || rotor_pos[2] > 26) { out = out.concat("* Rotor position 3: Please enter valid position<br>"); }
+    if (!rotor_pos[2]) { out = out.concat("Rotor position 3: Please enter a valid number<br>"); }
+    else if (rotor_pos[2] < 1 || rotor_pos[2] > 26) {
+        out = out.concat("Rotor position 3: Please enter valid position<br>"); }
 
     if (out) {
         document.getElementById('output').innerHTML = "Error:<br>" + out;
         return;
     };
 
-    var plugboard = document.getElementById("plugboard").value;
-    console.log(plugboard);
-    plugboard = plugboard.split(" ");
+    // ---------- plugboard ----------
+    // get the string
+    var plugboard = document.getElementById("plugboard").value.toUpperCase();
+    // replace spaces (for testing)
+    var pb_str = plugboard.replaceAll(" ", "");
+
+    // if the string length is odd -> error
+    if (pb_str.length % 2 == 1) {
+        document.getElementById('output').innerHTML = "Plugboard: Odd number of letters";
+        return;
+    }
+    // if there is duplicate letter -> error
+    var pb_tmp = [];
+    for (i in pb_str) {
+        i = pb_str[i];
+        if (!pb_tmp.includes(i)) { pb_tmp.push(i); }
+        else {
+            document.getElementById('output').innerHTML = "Plugboard: Duplicate letter (" + i + ")";
+            return;
+        }
+    }
+    // get the object
+    // pairs are transferred into the object
+    // eaxmple: poland -> {"p": "o", "o": "p", "l": "a", "a": "l", "n": "d", "d": "n"}
+    pb_arr = plugboard.split(" ");
+    plugboard = {};
+    for (pb in pb_arr) {
+        pb = pb_arr[pb];
+        plugboard[pb[0]] = pb[1];
+        plugboard[pb[1]] = pb[0];
+    }
 
 
     // ---------- en/decrypt ----------
@@ -89,6 +121,9 @@ function enigma(character) {
     rotor_pos[0]--;
     rotor_pos[1]--;
     rotor_pos[2]--;
+
+    // ----- plugboard -----
+    if (plugboard.hasOwnProperty(character)) { character = plugboard[character]; }
 
     // ----- 1st rotors -----
     character = rotors[rotor_sel[0]][(abc.indexOf(character) + rotor_pos[0]) % 26];
@@ -110,6 +145,9 @@ function enigma(character) {
     t = rotors[rotor_sel[0]].indexOf(character) - rotor_pos[0];
     if (t < 0) { t += 26; }
     character = abc[t];
+
+    // ----- plugboard -----
+    if (plugboard.hasOwnProperty(character)) { character = plugboard[character]; }
 
     // restore rotor positions
     rotor_pos[0]++;
@@ -144,5 +182,5 @@ function enigma(character) {
 
     // ---------- display character ----------
     document.getElementById('output').innerHTML = old_character + " = " + character;
-    return character;
+    //return character;
 }
